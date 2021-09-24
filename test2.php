67e0ac200457c7901fbde96fea9bcf4a18e83f0e
vicsaint <vic_snts@yahoo.com>
2021-09-24 17:37:42 +0800

    Update test2.php

--- test2.php
+++ test2.php
@@ -15,7 +15,7 @@ function HammingDistanceSolution($binary1, $binary2) {
     for ($i = 0; $i < count($a1); $i++) {
         
         if($a1[$i] != $a2[$i]) { //this is the distance that we are trying to solve
-             $hammingDistance++; //we measure by incrementing 1 place value of binary 1 does not match to binary 2
+             $hammingDistance++; //we measure by incrementing 1 if a place value of binary 1 does not match to binary 2
         }
     } 
     return $hammingDistance; // then we return the distance value
