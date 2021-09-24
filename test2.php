5824f505afff74737e940a767ff4f1bb353f4818
victor <it@ebos-sg.com>
2021-09-24 17:19:56 +0800

    Test 2 Hamming Distance

--- test2.php
+++ test2.php
@@ -10,15 +10,15 @@ function HammingDistanceSolution($binary1, $binary2) {
     $a2 = str_split($binary2);
    // print_r($a2); exit;
     
-    $dh = 0; //initial value zero
+    $hammingDistance = 0; //initial value zero
     
     for ($i = 0; $i < count($a1); $i++) {
         
-        if($a1[$i] != $a2[$i]) { //this is the distance that we are trying to solved
-             $dh++; //we measure by incrementing 1 of the precende order value of binary 1 does not match to binary 2
+        if($a1[$i] != $a2[$i]) { //this is the distance that we are trying to solve
+             $hammingDistance++; //we measure by incrementing 1 place value of binary 1 does not match to binary 2
         }
     } 
-    return $dh; // then we return the distance value
+    return $hammingDistance; // then we return the distance value
 }
 
 echo HammingDistanceSolution('0001','0100');
